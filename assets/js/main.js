075823db5df5ba2c6b8dff617ec54f7dfabe066b
Elizaveta <svillyfly@mail.ru>
2019-06-08 22:43:32 +0200

    started

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+
+
+    // Navigation
+    $("nav li a").click(function () {
+
+        $("nav li a").removeClass("active")
+        $(this).toggleClass("active")
+    });
+    // Scroll to the desired section on click
+    // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+    // Example: 
+    // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+    function scrollToSection(event) {
+        event.preventDefault();
+        var $section = $($(this).attr('href'));
+        $('html, body').animate({
+            scrollTop: $section.offset().top
+        }, 500);
+    }
+    $('[data-scroll]').on('click', scrollToSection);
+
+
+    
+
+
+});
